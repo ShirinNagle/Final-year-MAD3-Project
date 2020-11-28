7dda6bf3e4b0ae649e5fc8a4ef07ec658ff7b9d1
ShirinNagle <G00363458@gmit.ie>
2020-11-28 12:24:43 +0000

    In Game Menus
    Added in game menus, no functionality yet

--- Star Assault/Assets/PowerUp/Scripts/ScaleParticles.cs
+++ Star Assault/Assets/PowerUp/Scripts/ScaleParticles.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class ScaleParticles : MonoBehaviour {
-	void Update () {
-		GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
-	}
+	void Update()
+	{
+#pragma warning disable CS0618 // Type or member is obsolete
+        GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
+#pragma warning restore CS0618 // Type or member is obsolete
+                              //GetComponent<ParticleSystem>().main.startSize = transform.lossyScale.magnitude;
+    }
 }
