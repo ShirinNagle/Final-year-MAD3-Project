73c0a356a4f575b5751cf8a8609e16333f8f4513
ShirinNagle <G00363458@gmit.ie>
2020-12-01 12:11:45 +0000

    Added lasers to player
    Added more functionality to the player, Need to work on the players position on the screen as the player flies.

---
+++ Star Assault/Assets/__Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicPlayer : MonoBehaviour
+{
+    //may add a singleton
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);//don't destroy music player
+    }
+
+    void Start()
+    {
+        Invoke("LoadScene", 7f);
+
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Star Assault/Assets/__Scripts/PlayerBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] float xSpeed = 4.0f;   // default is in ms^-1
+    [SerializeField] float ySpeed = 4.0f;   // default is in ms^-1
+    [SerializeField] float hClamp = 10.0f;
+    [SerializeField] float vClamp = 7.0f;
+
+    [SerializeField] float controlPitchFactor = -30.0f;
+
+    [SerializeField] float positionPitchFactor = -1.0f;
+    [SerializeField] float positionYawFactor = -2.0f;
+    [SerializeField] float controlRollFactor = -30.0f;
+
+    float horizontalThrow, verticalThrow;
+    bool controlsActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlsActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controlsActive)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+
+    }
+
+    private void PlayerHasDied()  // call by string reference
+    {
+        controlsActive = false;
+    }
+
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + controlPitchFactor * verticalThrow; ;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = controlRollFactor * horizontalThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalThrow = Input.GetAxis("Horizontal");
+        verticalThrow = Input.GetAxis("Vertical");
+        float xOffset = horizontalThrow * xSpeed * Time.deltaTime;
+        float yOffset = verticalThrow * ySpeed * Time.deltaTime;
+
+        float xPos = Mathf.Clamp(transform.localPosition.x + xOffset, -hClamp, hClamp);
+        float yPos = Mathf.Clamp(transform.localPosition.y + yOffset, -vClamp, vClamp);
+
+        transform.localPosition = new Vector3(xPos,
+                                              yPos,
+                                              transform.localPosition.z);
+    }
+}
