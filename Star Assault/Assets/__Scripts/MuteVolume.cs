8f0a139e7b99a27acfa329041ed0401f51bda6b0
ShirinNagle <G00363458@gmit.ie>
2020-12-15 14:35:25 +0000

    Added final pdfs to project

---
+++ Star Assault/Assets/__Scripts/MuteVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteVolume : MonoBehaviour
+{
+    private bool isMuted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //isMuted = false;
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
+    }
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
+    }
+
+    
+}
+//https://www.youtube.com/watch?v=J-igitoipYY
